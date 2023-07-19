1f5b06a45621a04d1062d1245422aebc97c94af7
leenar24-meet <139689053+leenar24-meet@users.noreply.github.com>
2023-07-19 16:39:55 +0300

    dfaehfe
    sdpfiash

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -10,12 +10,39 @@ app = Flask(  # Create a flask app
 
 username = "siwarha"
 password = "123"
-facebook_friends=["Loai","Kenda","Avigail", "George", "Fouad", "Gi"]
+facebook_friends=["Nada","Samar","Leen", "Marcelle", "Fouad", "Gi"]
 
 
-@app.route('/')  # '/' for the default page
+
+
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+  if request.method== 'POST':
+  	if username==request.form['username'] and password== request.form['password']:
+  		return redirect(url_for('home'))
+  	else:
+  		return render_template('login.html')
+  		print("wrong password")
+  else:
+  	return render_template('login.html')
+
+
+@app.route('/home')
+def home():
+	return render_template('home.html', f1= facebook_friends)
+@app.route('/friend_exists/<string:name>')
+def friend_exists(name):
+	return render_template('friend_exists.html', n=name,  f1=facebook_friends)
+
+
+
+
+
+  		
+
+  		
+
+
   
 
 
